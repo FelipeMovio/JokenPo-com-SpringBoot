7d333ad6925a145f04892be0cbbfd8a1441fcd86
FelipeMovio <felipelsmovio@gmail.com>
2025-09-25 19:23:51 -0300

    ..

--- joken/src/main/java/com/felipeMovio/joken/dto/ResultadoResponse.java
+++ joken/src/main/java/com/felipeMovio/joken/dto/ResultadoResponse.java
@@ -1,4 +1,24 @@
 package com.felipeMovio.joken.dto;
 
+import com.felipeMovio.joken.model.Jogada;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@NoArgsConstructor
 public class ResultadoResponse {
+    private String resultadoRodada;
+
+    private Jogada jogadaComputador;
+
+    private Integer vitoriasJogador;
+    private Integer vitoriasComputador;
+
+    private boolean isFinalizado;
+
+    private String resultadoFinal;
 }

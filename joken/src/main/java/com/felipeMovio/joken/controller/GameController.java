4d9d156141a29ed858132d08eb77e0f2c5c1236a
FelipeMovio <felipelsmovio@gmail.com>
2025-09-27 12:44:17 -0300

    finalizando o controller

--- joken/src/main/java/com/felipeMovio/joken/controller/GameController.java
+++ joken/src/main/java/com/felipeMovio/joken/controller/GameController.java
@@ -16,4 +16,29 @@ public class GameController {
 
     private Game jogo;
 
- 
+    @PostMapping("/jogar")
+    public ResultadoResponse jogar(@RequestBody JogadaRequest Request) {
+        if (jogo.isFinalizado()){
+            return new ResultadoResponse(
+                    "o jogo ja terminol",
+                    null,
+                    jogo.getVitoriasJogador(),
+                    jogo.getVitoriasComputador(),
+                    true,
+                    jogo.resultado()
+            );
+        }
+        jogo.setJogadaJogador(Request.getJogada());
+        jogo.jogadaComputador();
+        String resultadoRodada = jogo.resultado();
+
+        return new ResultadoResponse(
+                resultadoRodada,
+                jogo.getComputador().getJogada(),
+                jogo.getVitoriasJogador(),
+                jogo.getVitoriasComputador(),
+                jogo.isFinalizado(),
+                jogo.isFinalizado() ? resultadoRodada : null
+        );
+    }
+}
